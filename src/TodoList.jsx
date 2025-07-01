66092c17738797003355c2ecfb7e1a0e6c2b7412
mbene dieye <mbenedieye757@gmail.com>
2025-07-01 11:17:26 +0200

    todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,9 @@
+import { useFetech } from "./hooks/useFetch"
+
 const TodoList = () =>{
 
+    const API_URL="https://jsonplaceholder.typicode.com/todos"
+    const {} = useFetech (API_URL,{method:"GET"})
     return(
         <>
         </>
