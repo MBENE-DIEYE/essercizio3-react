4c10e3d7c1597d390fe154897bd3255d0b8c740b
mbene dieye <mbenedieye757@gmail.com>
2025-07-09 16:23:50 +0200

    todolist

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -9,7 +9,7 @@ const TodoList = () => {
     const filter = useFilteredTodos(data,searchTerm)
     const [searchTerm, setSearchTerm] = useState()
     // const { data, loading, error } = useFetech(API_URL)
-    const [data, loading ,error] = useContext(TodoContext)
+    const [data, setData] = useContext(TodoContext)
     if (loading) {
 
         <p>caricamento...</p>
@@ -27,12 +27,13 @@ const TodoList = () => {
    
    },[data,searchTerm])
     useEffect(()=>{
+        setData()
         inputRef.current.focus()
     },[])
    
     return (
         <>
-            {/* {
+             {
                 data && data.map((item) => {
                     return (
                         <>
@@ -40,7 +41,7 @@ const TodoList = () => {
                             {
                                 <>
                                 <li>{item.id}</li>
-                                <li>{item.title}</li>
+                                <li>{item.text}</li>
                                 </>
                             }
                         </ul>
@@ -52,7 +53,7 @@ const TodoList = () => {
                 })
                 
                 
-            } */}
+            } 
 
             <form >
                 <label htmlFor="recerca">recerca</label>
